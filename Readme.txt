a6f383e71e4d31a26b1529db97992ac61d7a7e2e
dyule <dyule@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-08 18:18:58 +0000

    Fixed a type causing timeslots not to show.
    git-svn-id: https://plugins.svn.wordpress.org/timed-widget/tags/1.0.1@502350 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Readme.txt
@@ -0,0 +1,42 @@
+=== Timed Widget ===
+Tags: widget, time, schedule, reference, roaming
+Requires at least: 3.3
+Tested up to: 3.3.1
+Stable tag: 1.0.1
+
+A plugin for displaying different content on your site at different times of day.
+
+== Description ==
+
+The Timed Widget WordPress plugin provides a widget which can display different content at different times of day. For example, suppose you have a WordPress site for your radio station, and you want to display a badge that changes as the different shows come on the air. The Timed Widget plugin provides a way to set up a schedule for your shows, and display different images at different times.
+
+The Timed Widget plugin allows you to create CodeBlocks, which are snippets of text or HTML that you want to display (just like the Text widget in WordPress). You can then create a schedule, which is made up of TimeSlots. Each TimeSlot is associated with a CodeBlock, and gives a time of day to appear (say, 8:00 am to 11:00 am), a range of dates to appear during (July 27th – September 5th) and the days of the week to appear on (Monday, Wednesday and Friday). So, whichever CodeBlock is associated with this TimeSlot will appear on Mondays Wednesdays and Fridays from 8:00 to 11:00 between the dates of July 27th and September 5th.
+
+
+== Installation ==
+
+* Download/upload plugin to wp-content/plugins directory
+* Activate Plugin within Admin Dashboard.
+ 
+== Frequently Asked Questions ==
+
+Coming soon
+
+== Screenshots ==
+
+Coming soon
+
+== Changelog ==
+
+= 1.0.1 =
+Fixed bug where some users were not seeing the events they created on the calendar.
+
+= 1.0 =
+* First Version
+
+
+
+== Upgrade Notice ==
+
+None Yet
+
---
+++ tw-init.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * @package TimedWidget
+ * @version 0.1
+ */
+/*
+Plugin Name: Timed Widget
+Plugin URI: http://code.library.unbc.ca/timed-widget
+Description: Displays timed content
+Author: Daniel Yule
+Version: 1.0.1
+*/
+
+require_once 'tw-widget.php';
+require_once  'tw-settings.php';
+
+add_action('admin_menu', 'tw_settings_menu');
+
+
+function tw_settings_menu (){
+	$page = add_options_page("Timed Widget Settings", "Timed Widget", "manage_options", "tw_settings", "tw_settings_display");
+	add_action("admin_print_styles-".$page, "tw_add_css_js");
+	//delete_option("tw_codeblock_list");
+}
+
+function tw_initialize_plugin() {
+	$codeblocks = get_option("tw_codeblock_list", null);
+	
+	if($codeblock == null)
+	{
+		$codeblocks[0] = new codeblock("Default Codeblock", "");
+		add_option("tw_codeblock_list", $codeblocks);
+	}
+	
+	$schedules = get_option("tw_schedule_list", null);
+	
+	if($schedules == null)
+	{
+		$schedules[0] = new widget_schedule("Default Schedule");
+		add_option("tw_schedule_list", $schedules);
+	}
+	
+	$exceptions = get_option("tw_exception_list", null);
+	
+	if($exceptions == null)
+	{
+		$exceptions = array();
+		add_option("tw_exception_list", $exceptions);
+	}
+}
+
+function tw_add_css_js() {
+	wp_enqueue_style("jquery.smoothness.css", plugins_url(). '/timed-widget/css/smoothness/jquery-ui-1.8.11.custom.css', array(), false, "all");
+	wp_enqueue_style("jquery.weekcalendar.css", plugins_url(). '/timed-widget/css/jquery.weekcalendar.css', array(), false, "all");
+	wp_enqueue_style("jquery.contextMenu.css", plugins_url(). '/timed-widget/css/jquery.contextMenu.css', array(), false, "all");
+	wp_enqueue_style("colorpicker.css", plugins_url(). '/timed-widget/css/colorpicker.css', array(), false, "all");
+	wp_enqueue_style("timed-widget.css", plugins_url(). '/timed-widget/css/timed-widget.css', array(), false, "all");
+	
+	wp_enqueue_script("jquery.weekcalendar.js", plugins_url() . "/timed-widget/js/jquery.weekcalendar.js", array("jquery", "jquery-ui-core", "jquery-ui-widget", "jquery-ui-button", "jquery-ui-droppable", "jquery-ui-resizable"));
+	wp_enqueue_script("jquery.contextMenu.js", plugins_url() . "/timed-widget/js/jquery.contextMenu.js", array("jquery", "jquery-ui-core", "jquery-ui-widget"));
+	wp_enqueue_script("date.js", plugins_url() . "/timed-widget/libs/date.js", array());
+	wp_enqueue_script("jquery-ui-datepicker", plugins_url() . "/timed-widget/libs/jquery.ui.datepicker.js", array("jquery", "jquery-ui-core", "jquery-ui-widget"));
+	wp_enqueue_script("custom-colorpicker", plugins_url() . "/timed-widget/libs/colorpicker.js", array("jquery"));
+	wp_enqueue_script("jquery-ui-accordion", plugins_url() . "/timed-widget/libs/jquery.ui.accordion.js", array("jquery", "jquery-ui-core", "jquery-ui-widget"));
+	wp_enqueue_script("jquery.suggest.local", plugins_url() . "/timed-widget/js/jquery-suggest-local.js", array("jquery"));
+	wp_enqueue_script("tw.settings.js", plugins_url() . "/timed-widget/js/settings.js", array("jquery.weekcalendar.js", "date.js", "jquery.suggest.local","jquery-ui-datepicker", "jquery-ui-dialog", "jquery-ui-accordion", "jquery-ui-tabs", "custom-colorpicker"));
+	
+// 	delete_option('tw_timeslot_list');
+// 	add_option('tw_timeslot_list', array());
+// 	delete_option(('tw_exception_list'));
+// 	add_option('tw_exception_list', array());
+
+	
+	$codeblocks = get_option("tw_codeblock_list");
+	
+	reset($codeblocks);
+	 	
+	$first_block_id = key($codeblocks);
+	
+	$schedules = get_option("tw_schedule_list");
+	
+// 	$exceptions = get_option("tw_exception_list", array());
+	
+// 	$timeslots = get_option('tw_timeslot_list');
+// 	foreach($timeslots as $timeslot_id => $timeslot) {
+// 		if(!isset($exceptions[$timeslot_id]))
+// 			$exceptions[$timeslot_id] = array();
+// 	}
+	
+// 	update_option('tw_exception_list', $exceptions);
+	
+	reset($schedules);
+	 	
+	$first_schedule_id = key($schedules);
+	
+	$codeblock_json = json_encode($codeblocks);
+	
+	
+	wp_localize_script( 'tw.settings.js', 'tw_ajax', array(  'codeblocks' => $codeblock_json, 'current_block' => $first_block_id, 'current_schedule' => $first_schedule_id ) );
+}
+
+register_activation_hook(__FILE__, 'tw_initialize_plugin');
+
+add_action( 'wp_ajax_tw-ajax-schedule-add', 'tw_ajax_schedule_add' );
+add_action('wp_ajax_tw-ajax-schedule-update', 'tw_ajax_schedule_update');
+add_action('wp_ajax_tw-ajax-schedule-remove', 'tw_ajax_schedule_remove' );
+add_action( 'wp_ajax_tw-ajax-codeblock-add', 'tw_ajax_codeblock_add' );
+add_action( 'wp_ajax_tw-ajax-codeblock-save', 'tw_ajax_codeblock_save' );
+add_action( 'wp_ajax_tw-ajax-codeblock-remove', 'tw_ajax_codeblock_remove' );
+add_action( 'wp_ajax_tw-ajax-timeslot-add-update', 'tw_ajax_timeslot_add_update');
+add_action( 'wp_ajax_tw-ajax-timeslot-remove', 'tw_ajax_timeslot_remove');
+add_action( 'wp_ajax_tw-ajax-get-timeslot', 'tw_ajax_timeslot_get');
+add_action( 'wp_ajax_tw-ajax-list-events', 'tw_ajax_date_list');
+add_action( 'wp_ajax_tw-ajax-exception-add', 'tw_ajax_exception_add');
+add_action( 'wp_ajax_tw-ajax-exception-remove', 'tw_ajax_exception_remove');
+add_action( 'wp_ajax_nopriv_tw-ajax-output-codeblocks', 'tw_ajax_output_codeblocks');
+?>
